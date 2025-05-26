b0316e0c55dd8b2dc9abb931315a2284d1901dd4
jaemoon <woans5970@naver.com>
2025-05-26 14:17:13 +0900

    fix: 메인페이지 문의 컴포넌트 제거

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <HeroSection />
       <CategorySection categories={categories} />
       <FeatureSection />
-      <ContactForm />
+      {/* <ContactForm /> */}
       <Footer />
     </div>
   )
